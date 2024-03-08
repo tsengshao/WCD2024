7150c6181258e9615b13bc0699e0ef55fe8c8954
shao <shaoyu.ts@gmail.com>
2024-03-08 21:52:59 +0800

    add config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+dataPath="/data2/C.shaoyu/WCD2024/dat/"
+vvmPath="/data2/C.shaoyu/WCD2024/taiwanvvm/"
+
+expList = [
+  'tpe20050712cln',  'tpe20060721cln',  'tpe20090817cln',  'tpe20100802cln',\
+  'tpe20110616cln',  'tpe20110816cln',  'tpe20130723cln',  'tpe20140703cln',\
+  'tpe20050723cln',  'tpe20070830cln',  'tpe20090827cln',  'tpe20100803cln',\
+  'tpe20110702cln',  'tpe20110821cln',  'tpe20130807cln',  'tpe20140825cln',\
+  'tpe20060508cln',  'tpe20080715cln',  'tpe20100629cln',  'tpe20100912cln',\
+  'tpe20110723cln',  'tpe20120715cln',  'tpe20130825cln',  'tpe20060718cln',\
+  'tpe20090707cln',  'tpe20100630cln',  'tpe20110615cln',  'tpe20110802cln',\
+  'tpe20120819cln',  'tpe20140525cln']
+
